3b5f656d05fdfe807fcec7143128b04872a8350b
Bryson Bloom <brysonbloom@icloud.com>
2023-02-08 14:31:22 -0700

    update

--- app/Services/HousesService.js
+++ app/Services/HousesService.js
@@ -1,9 +1,16 @@
-import { sandboxApi } from "./AxiosService"
+import { sandboxApi } from "./AxiosService.js"
+import { appState } from "../AppState.js";
+import { House } from "../Models/House.js";
 
 class HousesService {
     async getHouses() {
         const res = await sandboxApi.get('/houses')
-        console.log(res.data);
+        // console.log(res.data);
+        const newArray = res.data.map(house => new House(house))
+        appState.houses = newArray
+        appState.emit(houses)
+        console.log('is this ting working');
+        // console.log(newArray);
     }
 }
 export const housesService = new HousesService()
