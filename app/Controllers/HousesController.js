0bfce2e1913a1d8ec5c7f83fb17a17bfc7da3132
Bryson Bloom <brysonbloom@icloud.com>
2023-02-08 14:31:41 -0700

    update

--- app/Controllers/HousesController.js
+++ app/Controllers/HousesController.js
@@ -1,16 +1,28 @@
+import { appState } from "../AppState.js";
 import { housesService } from "../Services/HousesService.js";
 import { Pop } from "../Utils/Pop.js";
+import { setHTML } from "../Utils/Writer.js";
+
+function _drawHouses() {
+    let template = ''
+    appState.houses.forEach(house => template += house.houseCard)
+    setHTML('listings', template)
+    console.log(template)
+}
 
 export class HousesController {
 
     constructor() {
-        this.getHouses()
+
+        appState.on('houses', _drawHouses())
     }
 
 
     async getHouses() {
+        console.log('is house button working?');
         try {
             await housesService.getHouses()
+            console.log('is try working');
         } catch (error) {
             Pop.error(error)
         }
--- app/Models/House.js
+++ app/Models/House.js
@@ -2,6 +2,7 @@ export class House {
     constructor(data) {
         this.id = data.id || ''
         this.bedrooms = data.bedrooms
+        this.imgUrl = data.imgUrl
         this.bathrooms = data.bathrooms
         this.levels = data.levels
         this.year - data.year
@@ -9,7 +10,24 @@ export class House {
         this.description = data.description
     }
 
-
+    get houseCard() {
+        return/*html */ `
+        <div class="col-md-4 mb-3">
+          <div class="card">
+            <img src="${this.imgUrl}" class="card-img-top car-img"
+              alt="house">
+            <div class="card-body">
+              <div class="card-title fs-5">${this.bedrooms + ' ' + this.bathrooms}</div>
+              <p>${this.description}</p>
+              <div class="d-flex justify-content-between">
+              <button class="btn ms-1 btn-danger" type="button" onclick="app.housesController.removeCar('${this.id}')">Delete Car!</button>
+              <button data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn ms-1 btn-info" type="button" onclick="app.carsController.drawForm('${this.id}')">Edit Car!</button>
+              </div>
+              </div>
+          </div>
+        </div>
+        `
+    }
 }
 
 
